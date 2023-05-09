efc7e715bb1c041d010ce3c179a7be1de89bd007
anhar <fndedek@gmailcom>
2023-05-09 10:57:49 +0700

    membuat form update berkas

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <div class="container">
-        <form action="/articles" method="post" enctype="multipart/formdata">
+        <form action="/articles" method="post" enctype="multipart/form-data">
         @csrf
             <div class="form-group">
                 <label for="title">Title: </label>
                 <input type="text" class="form-control" required="required" name="title"></br>
                 <label for="content">Content: </label>
                 <textarea type="text" class="form-control" required="required" name="content"></textarea></br>
-                <label for="image_n">Feature Image: </label>
+                <label for="image">Feature Image: </label>
                 <input type="file" class="form-control" required="required" name="image"></br>
                 <button type="submit" name="submit" class="btn btn-primaryfloat-right">Simpan</button>
             </div>
